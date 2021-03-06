7e31be59ad565e0fc979fadf3660ca4523677596
Lucas Viana (Bau) <contato@lucasviana.dev>
2021-03-06 19:26:07 -0300

    :feature: function to set file currencies.json

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,27 +4,32 @@ module Lib
     ( middlewareRequest
     ) where
 
-import Prelude as P
-
-import Data.Map.Strict as STS
-
+import Data.Aeson as AS ( decode, encode )
+import Data.Bifunctor ( Bifunctor(second, first) )
+import Data.ByteString.Lazy.UTF8 as BSU ( fromString, toString )
+import Data.List as L ( (++), find )
+import Data.Map.Strict as STS ( lookup, toList, fromList, Map )
 import Data.Maybe (fromMaybe)
-
-import Data.Bifunctor (second)
-
-import Control.Monad.IO.Class
-
-import Data.ByteString.Lazy.UTF8 as BSU
-
-import Data.Text
-
-import Data.List as L
-
-import Data.Aeson as AS
+import Data.Text ( pack, Text )
 import Network.HTTP.Req
+    ( (/:),
+      defaultHttpConfig,
+      https,
+      jsonResponse,
+      req,
+      responseBody,
+      runReq,
+      GET(GET),
+      NoReqBody(NoReqBody) )
+import Prelude as P
+import Types.BTC ( BTC(BTC), BPI(BPI), Currency(Currency) )
 
-import Types.BTC
-import qualified Data.Bifunctor
+data AvailableCurrencies = EUR | BRL | CAD deriving Show
+
+data CurrencyValue = CurrencyValue { currency :: AvailableCurrencies, value :: Float  }
+
+pathFileCurrencies :: FilePath
+pathFileCurrencies = "assets/currencies.json"
 
 listOfCurrencyLabels :: [(String, String)]
 listOfCurrencyLabels = [
@@ -46,12 +51,15 @@ calculateRateFloat btcRate (code, rate) =  (textCode, Currency textCode rateText
 getCurrencyLabelByCode :: String -> Maybe String
 getCurrencyLabelByCode code = fmap snd (L.find (\(code',_) -> code' == code) listOfCurrencyLabels)
 
-convertToFloat :: String -> Float
-convertToFloat = read
+stringToFloat :: String -> Float
+stringToFloat = read
+
+floatToString :: Float -> String
+floatToString = show
 
 readFileCurrencies :: IO [(String, Float)]
 readFileCurrencies = do
-  currencies <- readFile "assets/currencies.json"
+  currencies <- readFile pathFileCurrencies
 
   let mJson = AS.decode $ BSU.fromString currencies :: Maybe (Map String String)
 
@@ -59,7 +67,7 @@ readFileCurrencies = do
     Just currencies' -> do
       let currenciesList = STS.toList currencies'
 
-      return (P.map (second convertToFloat) currenciesList)
+      return (P.map (second stringToFloat) currenciesList)
     _   -> error "Arquivo com formato incompatível"
 
 
@@ -76,13 +84,44 @@ middlewareRequest = do
 
     case mUsd of
       Just usd -> do
-        let mBtc = STS.lookup "BTC" bpi
-        
-        case mBtc of
-          Just btc -> do
-            let (Currency _ _ _ btcRate) = usd
-            let baseList' = P.map (calculateRateFloat btcRate) file
+        let placeholderBtc = pack (fromMaybe "" (getCurrencyLabelByCode "BTC"))
 
-            liftIO (return (("USD", usd) : ("BTC", btc) : baseList'))
-          _ -> error "Moeda BTC não retornado pela API"
+        let btc = fromMaybe (Currency "BTC" "1" placeholderBtc 1) (STS.lookup "BTC" bpi)
+
+        let (Currency _ _ _ btcRate) = usd
+        let baseList' = P.map (calculateRateFloat btcRate) file
+
+        let newList  = STS.fromList (P.map (Data.Bifunctor.first show) baseList')
+
+        return (("USD", usd) : ("BTC", btc) : baseList')
       _ -> error "Moeda USD não retornado pela API"
+
+parseValuesToString :: [(String, Float)] -> [(String, String)]
+parseValuesToString = P.map (second floatToString)
+
+setCurrencyFileValue :: CurrencyValue -> IO (Either String Bool)
+setCurrencyFileValue (CurrencyValue code newvalue) = do
+    currencies <- readFileCurrencies
+
+    let mCurrency = L.find (\i -> fst i == show code) currencies
+
+    let parseFile x = BSU.toString (encode $ fromList (parseValuesToString x))
+
+    case mCurrency of
+      Just _ -> do
+        if newvalue < 0 then 
+          return $ Left "Valor não pode ser negativo"
+        else do
+          let changeCurrencyValue (x, y) = (x, if x == show code then newvalue else y)
+
+          let newFile = parseFile (P.map changeCurrencyValue currencies)
+          
+          writeFile pathFileCurrencies newFile
+
+          return $ Right True
+      _ -> do
+        let newFile = parseFile (currencies ++ [(show code, newvalue)])
+
+        writeFile pathFileCurrencies newFile
+
+        return $ Right True
